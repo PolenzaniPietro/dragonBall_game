74d44d8a155212990f19f843e5b2ce945f4ca97a
PolenzaniPietro <polenzani.pietro@franchettisalviani.net>
2026-03-30 08:39:43 +0200

    creata classe per il salvataggio della partita

---
+++ dragonBallGame/src/dragonballgame/GameSaver.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dragonballgame;
+
+/**
+ *
+ * @author polenzani.pietro
+ */
+public class GameSaver {
+    private Character c;
+    private Player p; 
+}
